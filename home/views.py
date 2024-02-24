4bf2a173232389f82e92d399868a9534af90f3a8
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-24 19:51:13 +0000

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home_page(request):
+    return HttpResponse("This will be the home page")
