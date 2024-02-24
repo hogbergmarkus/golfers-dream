8374918e3f57c7fd072927745c4e17a271f254c8
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-24 20:21:11 +0000

    Secure SECRET KEY

--- golfers_dream/settings.py
+++ golfers_dream/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g8o!3q-^wot!+zi^-59&=mzz17s^+k$0g0^v1pawr+h1w((h04'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
