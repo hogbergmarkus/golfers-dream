f97d417c69341376eacd775ae9fd53b3dba36481
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-26 12:51:39 +0000

    Add CustomUserAdmin to handele users in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Customize the admin panel for displaying users.
+    Add new fields to the Add user interface in Admin panel.
+    """
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'user_type'
+    )
+
+    # Define the look of admin page when displaying users
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('User Type', {'fields': ('user_type',)}),
+
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+            )}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Define fields of add user on admin page
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'first_name',
+                'last_name',
+                'username',
+                'email',
+                'password1',
+                'password2'
+                ),
+        }),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -10,8 +10,13 @@ USER_TYPES = (
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    """
+    Add a custom user model
+    """
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    user_type = models.CharField(max_length=2, choices=USER_TYPES, default='GU')
+    user_type = models.CharField(
+        max_length=2, choices=USER_TYPES, default='GU'
+        )
