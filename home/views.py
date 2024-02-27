96831d8c989cfacf0032f62d36c815b8ce67b2f6
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-27 09:09:28 +0000

    Add some comments

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """
+    Renders the  home/index.html template.
+    """
     return render(request, 'home/index.html')
