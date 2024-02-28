33a3f87cd1e8db3b4bedad1c8a76e3b5cc02e731
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-28 09:59:31 +0000

    Add about.html, connect view and urls

--- home/views.py
+++ home/views.py
@@ -7,3 +7,10 @@ def index(request):
     Renders the  home/index.html template.
     """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    Render the home/about.html template.
+    """
+    return render (request, 'home/about.html')
