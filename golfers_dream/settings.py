c0c9f099750ed599f9947252bb090a6b2f768566
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-28 14:02:43 +0000

    Pushed with debug true, set to false now

--- golfers_dream/settings.py
+++ golfers_dream/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-hogbergmark-golfersdrea-6bj3a748gwi.ws-eu108.gitpod.io',
