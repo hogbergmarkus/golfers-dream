d5f92cb78829e4ea355e546a27a7904e8bda5dec
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-29 14:44:48 +0000

    Add view to the booking app

--- booking/views.py
+++ booking/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import BookingForm
-from .models import Booking
 
 
 # Create your views here.
 def booking(request):
     """
-    Render the booking/booking.html template.
+    Render the booking/booking.html template,
+    and the BookingForm.
     """
-    return render(request, 'booking/booking.html')
+    if request.method == "POST":
+        booking_form = BookingForm(data=request.POST)
+        if booking_form.is_valid():
+            booking_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Reservation received! Approval pending.'
+            )
+    
+    booking_form = BookingForm()
+
+    return render(
+        request,
+        'booking/booking.html',
+        {
+            'booking': booking,
+            'booking_form': booking_form
+        },
+    )
