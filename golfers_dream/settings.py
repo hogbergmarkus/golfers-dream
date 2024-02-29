40834e6a07dcb52eeabe38566f74fd4d99575c9f
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-29 10:28:07 +0000

    Create a custum sign up form

--- golfers_dream/settings.py
+++ golfers_dream/settings.py
@@ -135,6 +135,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+ACCOUNT_FORMS = {'signup': 'user_profiles.forms.CustomSignupForm'}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
---
+++ user_profiles/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from allauth.account.forms import SignupForm
+
+
+class CustomSignupForm(SignupForm):
+    """
+    Extends the allauth signup form with additional fields.
+    """
+    first_name = forms.CharField(max_length=50, required=True, label='First Name')
+    last_name = forms.CharField(max_length=50, required=True, label='Last Name')
+    username = forms.CharField(max_length=50, required=True, label='Username')
+    email = forms.EmailField(max_length=50, required=True, label='Email')
+    
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
+
+        user.save()
+        return user
