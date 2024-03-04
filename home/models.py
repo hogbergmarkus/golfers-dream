e447ccd60c5096eace8b896f1dc519989e4632d5
Markus Högberg <m.o.hogberg@gmail.com>
2024-03-04 11:00:05 +0000

    Create model ContactMessage

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField(blank=False)
+    mark_as_read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
