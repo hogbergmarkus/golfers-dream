818d58fc8ccd5c45dfc1eb4a528676441546a9b5
Markus Högberg <m.o.hogberg@gmail.com>
2024-03-04 11:53:31 +0000

    Create view for contact

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactMessageForm
 
 
 # Create your views here.
@@ -14,3 +16,31 @@ def about(request):
     Render the home/about.html template.
     """
     return render (request, 'home/about.html')
+
+
+def contact(request):
+    """
+    Render the home/contact.html template,
+    and the ContactMessageForm.
+    """
+    if request.method == "POST":
+        contact_form = ContactMessageForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Your message has been received!'
+            )
+            return redirect('home')
+
+    contact_form = ContactMessageForm()
+
+    return render(
+        request,
+        'home/contact.html',
+        {
+            'contact': contact,
+            'contact_form': contact_form
+        },
+    )
