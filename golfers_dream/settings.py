21f5ec7fe65eda4494810ae7c68f5403faa1dcf5
Markus Högberg <m.o.hogberg@gmail.com>
2024-03-13 18:28:29 +0000

    Remove commented out code

--- golfers_dream/settings.py
+++ golfers_dream/settings.py
@@ -104,12 +104,6 @@ WSGI_APPLICATION = 'golfers_dream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
